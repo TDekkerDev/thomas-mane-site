bdc8722fe693e5df4128de58a84a0400d15905c7
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-01-09 22:53:27 +0100

    w

--- skills/skills_2.php
+++ skills/skills_2.php
@@ -6,5 +6,16 @@
 </head>
 <body>
 <?php include '../include/navbar_skills.php'; ?>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5029091454572166"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-5029091454572166"
+     data-ad-slot="1916344649"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 </body>
 </html>
