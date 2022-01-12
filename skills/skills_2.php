49e418298636999b297f0e93574d3da2e17ebffe
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-01-12 16:25:07 +0100

    ww

--- skills/skills_2.php
+++ skills/skills_2.php
@@ -2,20 +2,34 @@
 <html lang="en">
 <head>
 <?php include '../include/head_skills.php'; ?>
+<link rel="stylesheet" href="../css/Wavy_Circle.css"> 
     <title>skills_2</title>
 </head>
 <body>
 <?php include '../include/navbar_skills.php'; ?>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5029091454572166"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-5029091454572166"
-     data-ad-slot="1916344649"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<div class="container">
+<div class="loader">
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+     </div>
+     </div>
 </body>
 </html>
