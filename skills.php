bb5a0b836db6e61bf59be3025cb6a0b04fb6f8ed
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-01-28 20:04:05 +0100

    554566

--- skills.php
+++ skills.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/skills.css">
 <?php include 'include/head.php'; ?>
     <title>skills</title>
 </head>
 <body>
 <?php include 'include/navbar.php'; ?>
-
+    <form action="skills/skills_1.php" method="POST">
+        <button class="button" style="vertical-align:middle" type="submit"><span>Skill 1</span></button>
+    </form>
+    <br/>
+    <form action="skills/skills_2.php" method="POST">
+        <button class="button"style="vertical-align:middle" type="submit"><span>Skill 2</span></button>
+    </form>
+    <br/>
+    <form action="skills/skills_3.php" method="POST">
+        <button class="button" style="vertical-align:middle" type="submit"><span>Skill 3</span></button>
+    </form>
+    <br/>
+    <form action="skills/skills_4.php" method="POST">
+        <button class="button"style="vertical-align:middle" type="submit"><span>Skill 4</span></button>
+    </form>
+    <br/>
+    <form action="skills/skills_5.php" method="POST">
+        <button class="button" style="vertical-align:middle" type="submit"><span>Skill 5</span></button>
+    </form>
+    <br/>
+    <form action="skills/admin.php" method="POST">
+        <button class="button" style="vertical-align:middle" type="submit"><span>admin</span></button>
+    </form>
 </body>
 </html>
