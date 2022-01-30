b59964d37069799d7514bdea044667921975dd7a
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-01-30 22:04:27 +0100

    test

--- about.php
+++ about.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="css/about.css">
 <?php include 'include/head.php'; ?>
     <title>about</title>
 </head>
 <body>
-    <link rel="stylesheet" href="css/about.css">
  <?php include 'include/navbar.php'; ?>
- <div class="Fade_scroll_items">
+      <div class="h2_main">
+      <div class="spacer_2"></div>
      <h2>Lorem ipsum 1</h2>
-     <img class="img_not" src="img/ThisPersonDoesNotExist/image-_1_.png" height= "200" width= "200"/>
+      </div>
+     <div class="Fade_scroll_items">
+     <br/>
      <div class="Fade">
-     <article>
+        <div class="spacer"></div>
+     <article id="jaja">
         Lorem ipsum dolor sit amet,consectetur adipiscing elit.
         Donec turpis augue,placerat malesuada justo lobortis,molestie rhoncus nunc.
         Duis aliquam enim eu lectus luctus faucibus.
@@ -27,88 +31,12 @@
         Aliquam eu dolor pretium, euismod lectus rutrum, congue purus.
         Donec imperdiet tristique tortor euismod tempor.
      </article>
+     <div class="spacer"></div>
+     <img class="img_not" src="img/ThisPersonDoesNotExist/image-_1_.png" height= "200" width= "200"/>
      </div>
+     
 </div>
-<div class="Fade_scroll_items">
-     <h2>Lorem ipsum 2</h2>
-     <img class="img_not" src="img/ThisPersonDoesNotExist/image-_2_.png" height= "200" width= "200"/>
-     <article>
-        Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-        Donec turpis augue,placerat malesuada justo lobortis,molestie rhoncus nunc.
-        Duis aliquam enim eu lectus luctus faucibus.
-        Duis et turpis sed leo bibendum volutpat eget ut justo.
-        Sed tempus elementum libero, ac vestibulum felis luctus vitae.
-        Integer aliquet malesuada felis, vel elementum est egestas ac.
-        Sed a commodo ante. Phasellus consequat urna diam, a gravida turpis aliquet nec.
-        Mauris quis molestie sem, at condimentum diam. Aenean efficitur tortor orci, quis pellentesque ex fermentum tincidunt.
-        Duis pharetra sapien non molestie volutpat. Cras quis nisi ut turpis laoreet tincidunt sit amet porttitor odio.
-        Nulla faucibus nisl eu justo iaculis tincidunt.
-        Fusce tincidunt nunc tincidunt, scelerisque sem vitae, cursus augue.
-        Phasellus ac nisl magna.
-        Aliquam eu dolor pretium, euismod lectus rutrum, congue purus.
-        Donec imperdiet tristique tortor euismod tempor.
-     </article>
-</div>
-<div class="Fade_scroll_items">
-     <h2>Lorem ipsum 3</h2>
-     <img class="img_not" src="img/ThisPersonDoesNotExist/image-_3_.png" height= "200" width= "200"/>
-     <article>
-        Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-        Donec turpis augue,placerat malesuada justo lobortis,molestie rhoncus nunc.
-        Duis aliquam enim eu lectus luctus faucibus.
-        Duis et turpis sed leo bibendum volutpat eget ut justo.
-        Sed tempus elementum libero, ac vestibulum felis luctus vitae.
-        Integer aliquet malesuada felis, vel elementum est egestas ac.
-        Sed a commodo ante. Phasellus consequat urna diam, a gravida turpis aliquet nec.
-        Mauris quis molestie sem, at condimentum diam. Aenean efficitur tortor orci, quis pellentesque ex fermentum tincidunt.
-        Duis pharetra sapien non molestie volutpat. Cras quis nisi ut turpis laoreet tincidunt sit amet porttitor odio.
-        Nulla faucibus nisl eu justo iaculis tincidunt.
-        Fusce tincidunt nunc tincidunt, scelerisque sem vitae, cursus augue.
-        Phasellus ac nisl magna.
-        Aliquam eu dolor pretium, euismod lectus rutrum, congue purus.
-        Donec imperdiet tristique tortor euismod tempor.
-     </article>
-</div>
-<div class="Fade_scroll_items">
-     <h2>Lorem ipsum 4</h2>
-     <img class="img_not" src="img/ThisPersonDoesNotExist/image-_4_.png" height= "200" width= "200"/>
-     <article>
-        Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-        Donec turpis augue,placerat malesuada justo lobortis,molestie rhoncus nunc.
-        Duis aliquam enim eu lectus luctus faucibus.
-        Duis et turpis sed leo bibendum volutpat eget ut justo.
-        Sed tempus elementum libero, ac vestibulum felis luctus vitae.
-        Integer aliquet malesuada felis, vel elementum est egestas ac.
-        Sed a commodo ante. Phasellus consequat urna diam, a gravida turpis aliquet nec.
-        Mauris quis molestie sem, at condimentum diam. Aenean efficitur tortor orci, quis pellentesque ex fermentum tincidunt.
-        Duis pharetra sapien non molestie volutpat. Cras quis nisi ut turpis laoreet tincidunt sit amet porttitor odio.
-        Nulla faucibus nisl eu justo iaculis tincidunt.
-        Fusce tincidunt nunc tincidunt, scelerisque sem vitae, cursus augue.
-        Phasellus ac nisl magna.
-        Aliquam eu dolor pretium, euismod lectus rutrum, congue purus.
-        Donec imperdiet tristique tortor euismod tempor.
-     </article>
-</div>
-<div class="Fade_scroll_items">
-     <h2>Lorem ipsum 5</h2>
-     <img class="img_not" src="img/ThisPersonDoesNotExist/image-_5_.png" height= "200" width= "200"/>
-     <article>
-        Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-        Donec turpis augue,placerat malesuada justo lobortis,molestie rhoncus nunc.
-        Duis aliquam enim eu lectus luctus faucibus.
-        Duis et turpis sed leo bibendum volutpat eget ut justo.
-        Sed tempus elementum libero, ac vestibulum felis luctus vitae.
-        Integer aliquet malesuada felis, vel elementum est egestas ac.
-        Sed a commodo ante. Phasellus consequat urna diam, a gravida turpis aliquet nec.
-        Mauris quis molestie sem, at condimentum diam. Aenean efficitur tortor orci, quis pellentesque ex fermentum tincidunt.
-        Duis pharetra sapien non molestie volutpat. Cras quis nisi ut turpis laoreet tincidunt sit amet porttitor odio.
-        Nulla faucibus nisl eu justo iaculis tincidunt.
-        Fusce tincidunt nunc tincidunt, scelerisque sem vitae, cursus augue.
-        Phasellus ac nisl magna.
-        Aliquam eu dolor pretium, euismod lectus rutrum, congue purus.
-        Donec imperdiet tristique tortor euismod tempor.
-     </article>
-</div>
+
     
 </body>
 </html>
